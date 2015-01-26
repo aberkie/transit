7207c4591f52148faef451e18276cb7dfad53c65
aberkie <aberkowitz@aaron-2.browsermedia.com>
2015-01-26 17:04:40 -0500

    Removing sample date

--- services/Transit_LineService.php
+++ services/Transit_LineService.php
@@ -28,18 +28,6 @@ class Transit_LineService extends BaseApplicationComponent
 		
 		$incidents = craft()->transit_api->call($service, $method);
 		$incidents = $incidents['Incidents'];
-		$incidents = array(array(
-			"DateUpdated"=> "2010-07-29T14:21:28",
-			"DelaySeverity"=> null,
-			"Description"=> "Red Line: Expect residual delays to Glenmont due to an earlier signal problem outside Forest Glen.",
-			"EmergencyText"=> null,
-			"EndLocationFullName"=> null,
-			"IncidentID"=> "3754F8B2-A0A6-494E-A4B5-82C9E72DFA74",
-			"IncidentType"=> "Delay",
-			"LinesAffected"=> "RD;",
-			"PassengerDelay"=> 0,
-			"StartLocationFullName"=> null
-		));
 		return $incidents;
 	}
 	
