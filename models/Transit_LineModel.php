280b3ddf812fec273f4fe455c37ab59e6ef6b08a
aberkie <aberkowitz@aaron-2.browsermedia.com>
2015-01-27 13:59:44 -0500

    Matching attribute names to API response

--- models/Transit_LineModel.php
+++ models/Transit_LineModel.php
@@ -7,12 +7,12 @@ class Transit_LineModel extends BaseModel
 	protected function defineAttributes()
 	{
 		return array(
-			'display_name' => AttributeType::String,
-			'end_station_code' => AttributeType::String,
-			'internal_destination_1' => AttributeType::String,
-			'internal_destination_2' => AttributeType::String,
-			'line_code' => AttributeType::String,
-			'start_station_code' => AttributeType::String
+			'DisplayName' => AttributeType::String,
+			'EndStationCode' => AttributeType::String,
+			'InternalDestination1' => AttributeType::String,
+			'InternalDestination2' => AttributeType::String,
+			'LineCode' => AttributeType::String,
+			'StartStationCode' => AttributeType::String
 		);
 	}
 }
--- models/Transit_StationModel.php
+++ models/Transit_StationModel.php
@@ -4,24 +4,25 @@ namespace Craft;
 
 class Transit_StationModel extends BaseModel
 {
+	
 	protected function defineAttributes()
 	{
 		return array(
-			'address_city' => AttributeType::String,
-			'address_state' => AttributeType::String,
-			'address_street' => AttributeType::String,
-			'address_zip' => AttributeType::String,
-			'code' => AttributeType::String,
-			'display_name' => AttributeType::String,
-			'lat' => AttributeType::String,
-			'line_code_1' => AttributeType::String,
-			'line_code_2' => AttributeType::String,
-			'line_code_3' => AttributeType::String,
-			'line_code_4' => AttributeType::String,
-			'lon' => AttributeType::String,
-			'name' => AttributeType::String,
-			'station_together_1' => AttributeType::String,
-			'station_together_2' => AttributeType::String
+			'Address_City' => AttributeType::String,
+			'Address_State' => AttributeType::String,
+			'Address_Street' => AttributeType::String,
+			'Address_zip' => AttributeType::String,
+			'Code' => AttributeType::String,
+			'Display_Name' => AttributeType::String,
+			'Lat' => AttributeType::String,
+			'LineCode1' => AttributeType::String,
+			'LineCode2' => AttributeType::String,
+			'LineCode3' => AttributeType::String,
+			'LineCode4' => AttributeType::String,
+			'Lon' => AttributeType::String,
+			'Name' => AttributeType::String,
+			'StationTogether1' => AttributeType::String,
+			'StationTogether2' => AttributeType::String
 		);
 	}
 }
