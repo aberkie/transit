4070b55b97c5f0ce17dee851d63b2884e35a0bc5
aberkie <aberkowitz@aaron-2.browsermedia.com>
2015-01-27 13:59:25 -0500

    Adding Get Station Information call

--- variables/TransitVariable.php
+++ variables/TransitVariable.php
@@ -4,18 +4,24 @@ namespace Craft;
 
 class TransitVariable
 {
-	public function getAllStations()
-	{
-		return craft()->transit_station->getAllStations();
-	}
-	
 	public function getKey()
 	{
 		return craft()->transit_key->getKey();
 	}
 	
+	/* STATION VARIABLES */
+	public function getAllStations()
+	{
+		return craft()->transit_station->getAllStations();
+	}
+	
 	public function getNextTrains($station)
 	{
 		return craft()->transit_station->getNextTrains($station);
+	}
+	
+	public function getStationInformation($station)
+	{
+		return craft()->transit_station->getStationInformation($station);
 	}
 }
