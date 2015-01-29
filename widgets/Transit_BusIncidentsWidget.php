8192dabc3a3b5aa03848dfe2c639e0c3f31a26bb
aberkie <aberkowitz@aaron.browsermedia.com>
2015-01-29 15:13:27 -0500

    Fixing typo

--- widgets/Transit_BusIncidentsWidget.php
+++ widgets/Transit_BusIncidentsWidget.php
@@ -11,7 +11,7 @@ class Transit_BusIncidentsWidget extends BaseWidget{
 	
 	public function getBodyHtml()
 	{
-		$incidents = craft()->transit_incidents->getBusIncidents();
+		$incidents = craft()->transit_incident->getBusIncidents();
 		return craft()->templates->render('transit/_widgets/bus_incidents', array(
 			'incidents' => $incidents
 		));
